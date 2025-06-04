982acedc79615f622a3fbfe90ae11226ba4c17f0
smart <smart@digitalwitchng.online>
2025-06-04 21:28:48 +0100

    deploy infrastructure on aws

--- variable.tf
+++ variable.tf
@@ -107,7 +107,7 @@ variable "namecheap_username" {
 variable "namecheap_client_ip" {
   description = "Client IP for Namecheap API access"
   type        = string
-  default     = "18.144.99.37" # Replace with your actual client IP
+  default     = "3.101.24.8" # Replace with your actual client IP
 }
 
 #===========
