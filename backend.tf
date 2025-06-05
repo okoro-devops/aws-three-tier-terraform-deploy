ddc07916651e9418157bfeabd5c43a7f3fa3ee18
smart <smart@digitalwitchng.online>
2025-06-05 13:33:23 +0100

    provision infra

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "digitalwitchngbucket"
     key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "eu-north-1"
+    region = "us-west-1"
   }
 }
