7ea14813ca8c207c085a10cedf793c681ca00827
smart <smart@digitalwitchng.online>
2025-06-05 14:07:50 +0100

    creating cluster issuer

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -6,6 +6,12 @@ provider "helm" {
     }
 }
 
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 data "aws_eks_cluster_auth" "eks" {
     name = aws_eks_cluster.eks.name
 }
